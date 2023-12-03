537d6e6f061a0fbe9391e96881683b42971cd755
pedrolisboaa <phlisboa2000@gmail.com>
2023-12-03 19:37:56 -0300

    inicial do estudo

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Tecnologia
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'titulo', 'autor',
+
+
+@admin.register(Tecnologia)
+class TecnlogiaAdmin(admin.ModelAdmin):
+    list_display = 'nome', 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tecnologia(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=256)
+    conteudo = models.TextField()
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    tecnologia = models.ManyToManyField(Tecnologia)
+
+    def __str__(self):
+        return self.titulo
